5f6f18c5dc6d2e7773df806e0ee396f04302ee32
Trisha Quan <trishaquan@Trishas-MacBook-Pro.local>
2020-12-08 00:42:32 -0500

    Add functionality to easier switch between sample input and real input

--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,8 @@
-def parse_input(file_num, delimiter):
-    file = open(f"input/input{file_num}.txt", "r")
+def parse_input(file_num, delimiter, sample=False):
+    if sample:
+        filename = f"input/input{file_num}_sample.txt"
+    else:
+        filename = f"input/input{file_num}.txt"
+    file = open(filename, "r")
     input_text = file.read()
     return input_text.split(delimiter)
